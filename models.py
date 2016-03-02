ef47f2c23ee1c98ab8f786a8e6ca7a8d4cb939ff
romain.fontugne <romain.fontugne@gmail.com>
2016-03-02 15:08:24 +0900

    set the as number as primary key

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ASN(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(primary_key=True)
     name   = models.CharField(max_length=255)
 
     def __str__(self):
